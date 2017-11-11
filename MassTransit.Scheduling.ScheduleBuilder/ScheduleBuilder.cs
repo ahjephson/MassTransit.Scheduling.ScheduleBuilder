e32cca8393bc654ee4cb1aa91da8c9e77d217add
Unknown <ahjephson@gmail.com>
2017-11-11 14:27:29 +0000

    Add Schedule Builder

---
+++ MassTransit.Scheduling.ScheduleBuilder/ScheduleBuilder.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace MassTransit.Scheduling.ScheduleBuilder
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ScheduleBuilder
+    {
+        private readonly InternalSchedule _schedule;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="description"></param>
+        /// <returns></returns>
+        public static ScheduleBuilder Create(string description = null)
+        {
+            return new ScheduleBuilder(description);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="description"></param>
+        public ScheduleBuilder(string description = null)
+        {
+            _schedule = new InternalSchedule(description);
+        }
+
+        private class InternalSchedule : RecurringSchedule
+        {
+            public string TimeZoneId { get; internal set; }
+            public DateTimeOffset StartTime { get; internal set; }
+            public DateTimeOffset? EndTime { get; internal set; }
+            public string ScheduleId { get; internal set; }
+            public string ScheduleGroup { get; internal set; }
+            public string CronExpression { get; internal set; }
+            public string Description { get; internal set; }
+            public MissedEventPolicy MisfirePolicy { get; internal set; }
+
+            public InternalSchedule()
+            {
+            }
+
+            public InternalSchedule(string description)
+            {
+                Description = description;
+            }
+        }
+
+        internal void SetCronExpression(string cron) => _schedule.CronExpression = cron;
+
+        internal void SetScheduleId(string scheduleId) => _schedule.ScheduleId = scheduleId;
+
+        internal void SetScheduleGroup(string scheduleGroup) => _schedule.ScheduleGroup = scheduleGroup;
+
+        internal void SetDescription(string description) => _schedule.Description = description;
+
+        internal void SetMisfirePolicy(MissedEventPolicy misfirePolicy) => _schedule.MisfirePolicy = misfirePolicy;
+
+        internal void SetStartTime(DateTimeOffset startTime) => _schedule.StartTime = startTime;
+
+        internal void SetEndTime(DateTimeOffset endTime) => _schedule.EndTime = endTime;
+
+        internal void SetTimeZoneId(string timeZoneId) => _schedule.TimeZoneId = timeZoneId;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T Build<T>() where T : class, RecurringSchedule => _schedule as T;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public RecurringSchedule Build() => _schedule;
+    }
+}
---
+++ MassTransit.Scheduling.ScheduleBuilder/ScheduleBuilderExtensions.cs
@@ -0,0 +1,105 @@
+using System;
+
+namespace MassTransit.Scheduling.ScheduleBuilder
+{
+    public static class ScheduleBuilderExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="scheduleBuilder"></param>
+        /// <param name="cron"></param>
+        /// <returns></returns>
+        public static ScheduleBuilder WithCronExpression(this ScheduleBuilder scheduleBuilder, string cron)
+        {
+            scheduleBuilder.SetCronExpression(cron);
+            return scheduleBuilder;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="scheduleBuilder"></param>
+        /// <param name="interval"></param>
+        /// <returns></returns>
+        public static ScheduleBuilder WithIntervalInSeconds(this ScheduleBuilder scheduleBuilder, int interval)
+        {
+            scheduleBuilder.SetCronExpression($"0/{interval} 0 0 ? * * *");
+            return scheduleBuilder;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="scheduleBuilder"></param>
+        /// <param name="interval"></param>
+        /// <returns></returns>
+        public static ScheduleBuilder WithIntervalInMinutes(this ScheduleBuilder scheduleBuilder, int interval)
+        {
+            scheduleBuilder.SetCronExpression($"0 0/{interval} 0 ? * * *");
+            return scheduleBuilder;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="scheduleBuilder"></param>
+        /// <param name="description"></param>
+        /// <returns></returns>
+        public static ScheduleBuilder WithDescription(this ScheduleBuilder scheduleBuilder, string description)
+        {
+            scheduleBuilder.SetDescription(description);
+            return scheduleBuilder;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="scheduleBuilder"></param>
+        /// <param name="startTime"></param>
+        /// <returns></returns>
+        public static ScheduleBuilder StartAt(this ScheduleBuilder scheduleBuilder, DateTimeOffset startTime)
+        {
+            scheduleBuilder.SetStartTime(startTime);
+            return scheduleBuilder;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="scheduleBuilder"></param>
+        /// <param name="endTime"></param>
+        /// <returns></returns>
+        public static ScheduleBuilder EndAt(this ScheduleBuilder scheduleBuilder, DateTimeOffset endTime)
+        {
+            scheduleBuilder.SetEndTime(endTime);
+            return scheduleBuilder;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="scheduleBuilder"></param>
+        /// <param name="startTime"></param>
+        /// <param name="endTime"></param>
+        /// <returns></returns>
+        public static ScheduleBuilder WithTimes(this ScheduleBuilder scheduleBuilder, DateTimeOffset startTime, DateTimeOffset endTime)
+        {
+            scheduleBuilder.SetStartTime(startTime);
+            scheduleBuilder.SetEndTime(endTime);
+            return scheduleBuilder;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="scheduleBuilder"></param>
+        /// <param name="timeZoneId"></param>
+        /// <returns></returns>
+        public static ScheduleBuilder InTimeZone(this ScheduleBuilder scheduleBuilder, string timeZoneId)
+        {
+            scheduleBuilder.SetTimeZoneId(timeZoneId);
+            return scheduleBuilder;
+        }
+    }
+}
